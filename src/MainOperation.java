09827e8e3e054cf4e7b747c830e6075a6a6616b9
archana sudani <archanasudani@gmail.com>
2019-01-06 09:15:19 +0000

    My First Commit

---
+++ src/MainOperation.java
@@ -0,0 +1,17 @@
+import loveProgramming.Operations;
+
+public class MainOperation {
+    public static void main(String[] args) {
+
+        Operations Op1;
+        Op1 = new Operations();
+
+        int k = Op1.add(20,65);
+        System.out.println("Sum of two number is: " + k);
+
+        int z = Op1.sub(72,19);
+        System.out.println("Difference of two number is: " + z);
+    }
+
+}
+
---
+++ src/loveProgramming/Operations.java
@@ -0,0 +1,15 @@
+package loveProgramming;
+
+public class Operations {
+    public int add(int i, int j) {
+
+        int k=i+j;
+        return k;
+    }
+
+    public int sub(int x, int y){
+
+        int z= x-y;
+        return z;
+    }
+}
